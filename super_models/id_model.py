db17e127472f6e1873fb7cf1cda4e127cea7b643
iMac <helloted@live.com>
2017-08-19 12:55:01 +0800

    移除部分

--- super_models/id_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, BigInteger, ForeignKey,JSON,DateTime
-from super_models.database import Base
-
-
-class IdBox(Base):
-    __tablename__ = 'id_box'
-
-    name = Column(String(32), primary_key=True)
-    value = Column(BigInteger, nullable=False)
